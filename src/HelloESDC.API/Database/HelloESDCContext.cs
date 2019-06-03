a8932089e255fdcb8d2cca3496c76474bb2a7173
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2019-06-03 19:40:08 -0400

    Added Greetings to DB context

--- src/HelloESDC.API/Database/HelloESDCContext.cs
+++ src/HelloESDC.API/Database/HelloESDCContext.cs
@@ -1,10 +1,14 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using HelloESDC.API.Models;
 
 namespace HelloESDC.API.Database 
 {
     public class HelloESDCContext : DbContext
     {
+
+        public DbSet<Greeting> Greetings { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         { 
             optionsBuilder.UseNpgsql(System.Environment.GetEnvironmentVariable("DB_CONNECTION_STRING"));
