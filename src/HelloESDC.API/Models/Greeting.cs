bb4b98710818dc953cec8339cc1194ffa858c749
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2019-06-03 19:40:25 -0400

    Greeting model: make name primary key

--- src/HelloESDC.API/Models/Greeting.cs
+++ src/HelloESDC.API/Models/Greeting.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HelloESDC.API.Models
 {
     public class Greeting
     {
+        [Key]
         public string Name { get; set; }
 
         public string Message { get; set; }
