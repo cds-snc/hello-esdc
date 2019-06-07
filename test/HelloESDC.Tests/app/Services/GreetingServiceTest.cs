4cedefc702851d598fafc4ee83340e7a13bd99e4
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-06-07 16:59:45 -0400

    update namespace

--- test/HelloESDC.Tests/app/Services/GreetingServiceTest.cs
+++ test/HelloESDC.Tests/app/Services/GreetingServiceTest.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Text;
 using Xunit;
 
-namespace HelloESDC.Tests.App
+namespace HelloESDC.Tests.App.Services
 {
     /// <summary>
     /// The greeting controller test repository.
