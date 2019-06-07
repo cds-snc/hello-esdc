4ce7a11bd180a71a2c1991db0055b382439c9168
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-06-07 13:04:38 -0400

    Connect service to db (#18)
    * temporarily comment out the tests
    
    * bit of cleanup and connection to db

--- src/HelloESDC.API/Service/IGreetingService.cs
+++ src/HelloESDC.API/Service/IGreetingService.cs
@@ -14,22 +14,9 @@ public interface IGreetingService
     List<Greeting> GetAllItems();
 
     /// <summary>
-    /// Adds a greeting.
-    /// </summary>
-    /// <param name="item">The new greeting.</param>
-    /// <returns>Returns a greeting.</returns>
-    Greeting Add(Greeting item);
-
-    /// <summary>
     /// Get a greeting by identifier.
     /// </summary>
     /// <param name="id">The unique identifier.</param>
     /// <returns>Returns a greeting.</returns>
     Greeting GetById(Guid id);
-
-    /// <summary>
-    /// Removes a greeting.
-    /// </summary>
-    /// <param name="id">The unique identifier.</param>
-    void Remove(Guid id);
 }
