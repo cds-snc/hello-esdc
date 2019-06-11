ddbbb4c1c69afc4c3a9407040555008ddf2d18ed
mark.buchanan <mark.buchanan@hrsdc-rhdcc.gc.ca>
2019-06-11 11:27:12 -0400

    Fixed using statement order

--- test/HelloESDC.Tests/app/Controllers/GreetingControllerTest.cs
+++ test/HelloESDC.Tests/app/Controllers/GreetingControllerTest.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using FluentAssertions;
 using HelloESDC.API.Models;
 using Microsoft.AspNetCore.Mvc;
-using FluentAssertions;
 using Moq;
 using Xunit;
 
