f81b389e76175b57e5a2e7dc3639bb5b33875b93
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-06-18 15:18:27 -0400

    add CDTS WxT templates for GCWeb theming

---
+++ src/Web/WebSPA/public/helper/wet.js
@@ -0,0 +1,39 @@
+window.onload = function() {
+    reloadTemplate();
+  };
+  
+  function reloadTemplate(){
+    var language = document.getElementsByTagName("html")[0].getAttribute('lang');
+    var switchLanguage = language.startsWith('en') ? 'fr' : 'en';
+  
+    var defTop = document.getElementById("def-top");
+    defTop.outerHTML = wet.builder.appTop({
+      appName: [
+        {
+          href: "#",
+          text: (switchLanguage === 'en' ? "[FR] Title" : 'Hello ESDC')
+        }
+      ],
+      search: false,
+      lngLinks: [
+        {
+          lang: switchLanguage,
+          href: "?lang=" + switchLanguage,
+          text: (switchLanguage === 'en' ? 'English' : 'Français')
+        }
+      ],
+      showPreContent: false,
+      topSecMenu: false
+    });
+  
+    var defPreFooter = document.getElementById("def-preFooter");
+    defPreFooter.outerHTML = wet.builder.preFooter({
+      dateModified: "2019-03-29",
+      versionIdentifier: "0.0.4",
+      showPostContent: false
+    });
+  
+    var defFooter = document.getElementById("def-footer");
+    defFooter.outerHTML = wet.builder.appFooter({
+    });
+  }
